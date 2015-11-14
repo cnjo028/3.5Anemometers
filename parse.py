cb65fbb9e5d9448c2f521d88e51e2ba19883ce52
cnjo028 <cnjo028@gmail.com>
2015-11-13 16:44:03 -0800

    comments

--- parse.py
+++ parse.py
@@ -1,3 +1,6 @@
+#note: currently must be run in a directory that contains this code, a subdirectory that contains
+#the data called data, an empty directory called withHeaders, and an empty directory called processed.
+
 import numpy as np
 import pandas as pd
 import csv
@@ -44,28 +47,28 @@ class parse(object):
         os.chdir(self.path)
         self.files = np.array(os.listdir(self.path))
         #print 'Reorganizing the data...'
-        for f in self.files: 
+        #for f in self.files: 
             #print('\r>> progress : %i/%i' % (j + 1, self.files.shape[0])),
             #sys.stdout.flush()
             #time.sleep(2)
-            loc0 = np.array(um?)
-            loc1 = np.array(uh)
-            loc2 = np.array(er)
-            date = f['DATE\TIME']
-                for j in range(0,date.shape[1]):
-                    if blah < date < blah
-                        loc0[j] = ""
-                        loc1[j] = ""
-                        loc2[j] = ""
-                    else if ......
-                        loc0[j] = ""
-                        loc1[j] = ""
-                        loc2[j] = ""
-                    else ....
-                        loc0[j] = ""
-                        loc1[j] = ""
-                        loc2[j] = ""
-                end 
+            #loc0 = np.array(um?)
+            #loc1 = np.array(uh)
+            #loc2 = np.array(er)
+            #date = f['DATE\TIME']
+             #   for j in range(0,date.shape[1]):
+              #      if blah < date < blah
+               #         loc0[j] = ""
+                #        loc1[j] = ""
+                 #       loc2[j] = ""
+                  #  else if ......
+                   #     loc0[j] = ""
+                    #    loc1[j] = ""
+                     #   loc2[j] = ""
+                    #else ....
+                     #   loc0[j] = ""
+                      #  loc1[j] = ""
+                       # loc2[j] = ""
+                #end 
             
             #something like that
 
@@ -79,16 +82,7 @@ class parse(object):
         
     #just a test method ._.
     def test(self):
-        print self.files
-        os.chdir(self.homedir)
-        os.chdir(self.path)
-        a = open(self.files[1], 'rt')
-        p = pd.read_csv(a);
-        headers = np.array(["Date/Time","AN0Speed","AN0Gust","AN0Pulse","AN1Speed","AN1Gust","AN1Pulse","AN2Speed","AN2Gust","AN2Pulse","CNT0","CNT1","CNT2","Wdir(Not Used)","Analog0","WV0","WV1","TempC","WV2","Analog5","Analog6","Analog7","?(Not Used)"])[np.newaxis]
-        np.transpose(headers);
-        print a
-        print headers.shape
-        print p.shape
+        
         
 if __name__ == "__main__":
     h = parse();
