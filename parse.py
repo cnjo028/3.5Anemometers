0aaa3961651b29563c609f24dafeefda1ca30ba3
cnjo028 <cnjo028@gmail.com>
2015-11-24 11:49:03 -0800

    update

--- parse.py
+++ parse.py
@@ -13,8 +13,10 @@ class parse(object):
 
     def __init__(self):
         self.homedir = os.getcwd() #where this script is
-        self.path = self.homedir + '/data' #where the data is
-        self.files = np.array(os.listdir(self.path)) #list of files of data
+        self.datapath = self.homedir + '/data' #where the data is
+        self.files = np.array(os.listdir(self.datapath)) #list of files of data
+        self.prog = 0; #keeps track of progress
+        self.filestat = np.array(os.listdir(self.datapath))
 
     #Takes no parameters
     #Iterates through all files in a directory of data, and adds headers
@@ -22,14 +24,12 @@ class parse(object):
     #using pandas.
     def addHeaders(self,headers):
         j=0;
-        print 'Adding Headers...'
         for f in self.files: 
-            print('\r>> progress : %i/%i' % (j + 1, self.files.shape[0])),
-            sys.stdout.flush()
-            time.sleep(2)
             os.chdir(self.homedir)
-            os.chdir(self.path)
+            os.chdir(self.datapath)
             if self.files[j][0] != '.':
+                self.prog = self.prog + 1;
+                self.progress();
                 a = open(self.files[j], 'rt')
                 p = pd.read_csv(a);
                 np.transpose(headers);
@@ -37,9 +37,7 @@ class parse(object):
                 os.chdir(self.homedir + '/withHeaders')
                 np.savetxt(self.files[j],a, delimiter=',',fmt="%s")
                 j += 1;
-        time.sleep(2)
         os.chdir(self.homedir)
-        print '\ndone'
 
     #Takes no parameters
     #Iterates through directory of data and reorders the data. Outputs final files
@@ -48,10 +46,6 @@ class parse(object):
         self.path = self.homedir + '/withHeaders';
         os.chdir(self.path)
         self.files = np.array(os.listdir(self.path))
-        
-        files = os.listdir(os.getcwd() + '/withHeaders')
-        os.chdir(os.getcwd() + '/withHeaders')
-
         midrt0st = datetime.strptime("2011-11-07","%Y-%m-%d")
         midrt0ed = datetime.strptime("2012-05-01","%Y-%m-%d")
         roof2st = datetime.strptime("2011-11-07","%Y-%m-%d")
@@ -61,33 +55,35 @@ class parse(object):
         catrt2st = datetime.strptime("2012-03-31","%Y-%m-%d")
         catrt2ed = datetime.strptime("2012-05-01","%Y-%m-%d")
         catlt2st = datetime.strptime("2012-05-01","%Y-%m-%d")
-
         log1 = datetime.strptime("2011-11-07","%Y-%m-%d")
         log2mv = datetime.strptime("2012-05-01","%Y-%m-%d")
         j =0;
-        for f in files:
-            if files[j][0] != '.':
+
+        for f in self.files: 
+            loc0 = np.array((''), dtype=str);
+            loc1 = np.array((''), dtype=str);
+            loc2 = np.array((''), dtype=str);
+            if self.files[j][0] != '.':
+                sys.stdout.flush()
+                time.sleep(2)
                 a = pd.read_csv(f)
                 times = np.array([a['Date/Time']])
                 times = np.transpose(times)
                 s = datetime.strptime(times[j][0], "%Y-%m-%d %H:%M:%S")
-                loc0 = np.array((''), dtype=str);
-                loc1 = np.array((''), dtype=str);
-                loc2 = np.array((''), dtype=str);
                 for j in range(0,times.shape[0]):
-                    time = datetime.strptime(times[j][0],"%Y-%m-%d %H:%M:%S")
-                    if time < log1:
+                    tread = datetime.strptime(times[j][0],"%Y-%m-%d %H:%M:%S")
+                    if tread < log1:
                         loc0 = np.append(loc0,"front dome");
                         loc1 = np.append(loc1,"lower right");
                         loc2 = np.append(loc2,"lower left");
-                    elif time < log2mv:
+                    else:
                         loc0 = np.append(loc0,"middle right");
                         loc1 = np.append(loc1,"top right");
-                        if roof2st < time and time < roof2ed:
+                        if roof2st < tread and tread < roof2ed:
                             loc2 = np.append(loc2,"roof");
-                        elif stair2st < time and time < stair2ed:
+                        elif stair2st < tread and tread < stair2ed:
                             loc2 = np.append(loc2,"stairwell");
-                        elif catrt2st < time < catrt2ed:
+                        elif catrt2st < tread and tread < catrt2ed:
                             loc2 = np.append(loc2,"catwalk right");
                         else:
                             loc2 = np.append(loc2,"catwalk left");
@@ -106,20 +102,25 @@ class parse(object):
                 times = np.c_[times,loc2]
                 times = np.c_[times,a['AN2Speed']]
                 times = np.c_[times,a['WV2']]
+                os.chdir(self.homedir + '/processed')
                 np.savetxt(f,times, delimiter=',',fmt="%s")
-                a = pd.read_csv('um.csv')
-
-                #add the headers back
-        os.chdir(self.homedir + '/processed')
-        np.savetxt(self.files[j],a, delimiter=',',fmt="%s")
-        j += 1;
-        #time.sleep(2)
-        #print '\ndone'
+                filetemp = pd.read_csv(f)
+                np.savetxt(f,times, delimiter=',',fmt="%s")
+            j += 1;
+    #os.chdir(self.homedir)
+        self.addHeaders(["Date/Time","AN0Loc","AN0Speed","WV0","AN1Loc","AN1Speed","WV1","AN2Loc","AN2Speed","WV2"])
+        self.prog = self.prog + 1;
+        self.progress();
         
-    #just a test method ._.
-    #def test(self):
+    def progress(self):
+        print('\r>> progress : %i/%i' % (self.prog, self.filestat.shape[0]))
+        sys.stdout.flush()
+        time.sleep(2)
+
         
         
 if __name__ == "__main__":
     h = parse();
+    #headers = np.array()
     h.addHeaders(headers = np.array(["Date/Time","AN0Speed","AN0Gust","AN0Pulse","AN1Speed","AN1Gust","AN1Pulse","AN2Speed","AN2Gust","AN2Pulse","CNT0","CNT1","CNT2","Wdir(Not Used)","Analog0","WV0","WV1","TempC","WV2","Analog5","Analog6","Analog7","?(Not Used)"])[np.newaxis]);
+    #h.reorder();
